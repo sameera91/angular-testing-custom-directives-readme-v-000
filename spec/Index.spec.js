fb5e90f209bfb70b00be07edc18f785f60bf20c7
sameera91 <sameera.bachoti@gmail.com>
2016-07-04 21:12:12 -0400

    Done.

--- spec/Index.spec.js
+++ spec/Index.spec.js
@@ -14,4 +14,22 @@ describe('Directive Test', function() {
 		expect(count.getInnerHtml()).toEqual('Current count: 1');
 	});
 
+	describe('Directive Test', function() {
+    browser.get('http://localhost:8080');
+ 
+    var counter = element(by.css('.counter'));
+    var count = element(by.css('.counter__count'));
+ 
+    it('should have an initial 0 count', function () {
+        expect(count.getInnerHtml()).toEqual('Current count: 1');
+    });
+ 
+    it('should increment when we click on it', function () {
+        counter.click();
+ 
+        expect(count.getInnerHtml()).toEqual('Current count: 2');
+    });
+ 
+	});
+
 });
